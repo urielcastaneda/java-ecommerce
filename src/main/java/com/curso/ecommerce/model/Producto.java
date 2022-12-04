711e28f63439e3259b3053c84886ed43084610c4
uriel <urielcastaneda@misena.edu.co>
2022-12-04 10:13:46 -0500

    mi rprimer commit

---
+++ src/main/java/com/curso/ecommerce/model/Producto.java
@@ -0,0 +1,99 @@
+package com.curso.ecommerce.model;
+
+public class Producto {
+	private Integer id;
+	private String nombre;
+	private String descripcion;
+	private String imagen;
+	private String precio;
+	private String cantidad;
+	
+	
+	
+	public Producto() {
+		super();
+	}
+
+
+	public Producto(Integer id, String nombre, String descripcion, String imagen, String precio, String cantidad) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.imagen = imagen;
+		this.precio = precio;
+		this.cantidad = cantidad;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+
+	public String getImagen() {
+		return imagen;
+	}
+
+
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+
+	public String getPrecio() {
+		return precio;
+	}
+
+
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+
+
+	public String getCantidad() {
+		return cantidad;
+	}
+
+
+	public void setCantidad(String cantidad) {
+		this.cantidad = cantidad;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Producto [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", imagen=" + imagen
+				+ ", precio=" + precio + ", cantidad=" + cantidad + "]";
+	}
+
+
+	
+	
+}
+
